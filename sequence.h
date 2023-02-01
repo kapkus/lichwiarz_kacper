1df8a3d5414c45f85df14c06c74cec253c4495eb
Kacper Lichwiarz <kapko1999@gmail.com>
2023-02-01 13:16:54 +0100

    documentation

--- sequence.h
+++ sequence.h
@@ -3,19 +3,34 @@
 
 #include <iostream>
 
+//! @class Sequence
+//! \brief Klasa tworząca sekwencję z liczb w parametrach
+
 class Sequence {
 private:
     int x;
     int y;
 
 public:
+    //! \brief Tworzy nowy obiekt z danymi parametrami
+    //! @param x parametr x
+    //! @param y parametr y
     Sequence(int x, int y);
+    //! \brief Kopiuje dane z istniejacego obiektu do nowego obiektu
+    //! @param cp obiekt
     Sequence(Sequence const &cp);
+    //! \brief Destruktor
     ~Sequence();
 
+    //! \brief getter X
     int getx();
+    //! \brief getter Y
     int gety();
+
+    //! \brief liczaca sume sekwencji
+    //! \f[C_{i,j} = \sum\limits_{x=0}^{n-1} A_{b,i} * B_{z,x}\f]
     double seqAdd();
+    //! \brief Funkcja liczaca roznice sekwencji
     double seqSub();
 };
 
--- statistics.h
+++ statistics.h
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+//! @class Statistics
+//! \brief Klasa operująca na wprowadzonych danych 
+
 class Statistics {
 private:
     int x;
@@ -10,6 +13,13 @@ private:
     int ws;
 
 public:
+
+    //! \brief table wprowadzenia wartosci
+    //! | x | y | ws |
+    //! |---|---|----|
+    //! | 1 | 2 |  3 |
+    //! | 4 | 5 |  6 |
+    //! | 7 | 8 |  9 |
     Statistics(int x, int y, int ws);
     Statistics(Statistics const &cp);
     ~Statistics();
@@ -17,6 +27,8 @@ public:
     int getx();
     int gety();
     double stat1();
+    //! \brief funkcja statystyczna 2
+    //! \f$ \int_{a}^{b} f(x) dx \f$
     double stat2();
 };
 
